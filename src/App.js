7f734a4106ae66570bc2324352a96763e5820e0f
Adrien <adrien.le.g.ll@gmail.com>
2020-06-09 20:15:52 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.scss';
+import SoundButton from './soundButton/soundButton';
+import audioFiles from './audioFiles';
+
+const buttons = Object.entries(audioFiles).map(([idx, { title, fileName }]) => {
+	if (title === undefined) {
+		title = fileName;
+	}
+	return <SoundButton key={idx} sound={fileName} title={title}></SoundButton>;
+});
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header"></header>
+			<header className="App-header">BeastBox</header>
+			<main className="App-main">{buttons}</main>
 		</div>
 	);
 }
---
+++ src/audioFiles.js
@@ -0,0 +1,35 @@
+import geralt_fuck from './assets/geralt_fuck.mp3';
+import geralt_hum from './assets/geralt_hum.mp3';
+import La_Folie_Des_Grandeurs_ca_va_chauffer from './assets/La_Folie_Des_Grandeurs_ca_va_chauffer.mp3';
+import ok_one_slap_aouwww from './assets/ok_one_slap_aouwww.mp3';
+import weeeee_hahahaha from './assets/weeeee_hahahaha.mp3';
+import ya_rien_ici from './assets/ya_rien_ici.mp3';
+
+let audioFiles = [
+	{
+		title: "Y'a rien ici",
+		fileName: ya_rien_ici,
+	},
+	{
+		title: 'Aouwww',
+		fileName: ok_one_slap_aouwww,
+	},
+	{
+		title: 'Weeeee',
+		fileName: weeeee_hahahaha,
+	},
+	{
+		title: 'Voila la vieille',
+		fileName: La_Folie_Des_Grandeurs_ca_va_chauffer,
+	},
+	{
+		title: 'Fuck',
+		fileName: geralt_fuck,
+	},
+	{
+		title: 'Hum',
+		fileName: geralt_hum,
+	},
+];
+
+export default audioFiles;
